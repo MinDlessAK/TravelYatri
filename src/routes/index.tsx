e1abbeaf71cd79c1ce1e4812a0e0dbf1c7821da7
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-19 15:47:13 +0530

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,108 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
+import Navbar from "~/components/home/navbar";
 
-export default component$(() => {
+// import ImgBg from '~/media/bg.jpg?jsx';
+export default () => {
+  
   return (
     <>
-      <Hero />
-      <Starter />
+{/* 
+ <div class=" flex flex-col min-h-screen bg-gradient-to-r from-slate-900 to-slate-700">
 
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
+   <div class=" h-14  w-full  flex justify-between items-center p-3 gap-3 ">
+      <div class="">
+        <a href="#"  class="text-cyan-300 font-extrabold text-2xl ml-10 ">TravelYatri</a>
       </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
+       <div class="">
+        <ul >
+          <li class="lg:space-x-20 space-x-6 text-xl">
+            <a href="#"  class="text-white hover:underline hover:text-cyan-400">Home</a>
+            <a href="#"  class="text-white hover:underline hover:text-cyan-400">Trips</a>
+            <a href="#"  class="text-white hover:underline hover:text-cyan-400">Services</a>
+            <a href="#"  class="text-white hover:underline hover:text-cyan-400">Contact</a>
+          </li>
+         </ul>
+        </div>
+         <div class="mr-10">
+           <button id="form-open" class="block px-6 py-3 rounded-full bg-gradient-to-r from-sky-600 to-cyan-400 text-center text-white">Login</button>
           </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
+    </div>
+    <div class="flex grow justify-center items-center flex-col gap-3">
+      <div class="bg-white flex  rounded-xl w-80 flex-col gap-6 p-4">
+              <div class="font-bold text-2xl flex justify-center">
+                <h2>Login</h2>
+              </div>
+                <div>
+                 <input type="email" placeholder="Enter your email" required class="border-b focus:outline-none focus:border-black w-full" />
+                </div>
+            
+               <div>
+                 <input type="password" placeholder="Enter your password" required class="border-b focus:outline-none focus:border-black w-full" />
+                </div>
 
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
+               
+              <div class="flex justify-between">
+                 <span class="checkbox">
+                  <input type="checkbox" id="check"  class="mr-1"/>
+                  <label for="check">Remember me</label>
+                </span>
+                <a href="#" class="text-cyan-400 hover:underline">Forgot password?</a>
             </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
 
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
+
+            <div class="flex">
+                <button class="rounded-full bg-cyan-500 text-white w-full h-10">Login Now</button>
             </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
+            <div class="flex justify-center">
+              Don't have an account? 
+            <a href="#" id="signup" class="text-cyan-400 hover:underline ml-1">Signup</a>
+            </div>
+        </div>
+        <div class="bg-white flex  rounded-xl w-80 flex-col gap-6 p-4">
+
+              <div class="font-bold text-2xl flex justify-center">
+                <h2>Signup</h2>
+              </div>
+
+                <div>
+                 <input type="email" placeholder="Enter your email" required class="border-b focus:outline-none focus:border-black w-full" />
+                </div>
+            
+               <div>
+                 <input type="password" placeholder="Create password" required class="border-b focus:outline-none focus:border-black w-full" />
+                </div>
+
+            <div>
+                 <input type="password" placeholder="Confirm password" required class="border-b focus:outline-none focus:border-black w-full" />
+            </div>
+
+            <div class="flex">
+                <button class="rounded-full bg-cyan-500 text-white w-full h-10">Signup Now</button>
+            </div>
+
+            <div class="flex justify-center">
+              Already  have an account? 
+            <a href="#" id="signup" class="text-cyan-400 hover:underline ml-1">Login</a>
+            </div>
         </div>
       </div>
-    </>
-  );
-});
+    
+  </div> */}
 
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+
+
+  <div class="flex flex-col min-h-screen bg-gradient-to-r from-slate-900 to-slate-700">
+  <Navbar></Navbar>
+   
+    <div class="flex grow justify-center items-center flex-col gap-3">
+      
+    </div>
+  </div>
+
+  </>
+  );
 };
+
+  
+
+
