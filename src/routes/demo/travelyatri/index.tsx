5f21eebdfd195ea29ccecc97ffd1cd3fb2e62182
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-23 13:32:03 +0530

    index

--- src/routes/demo/travelyatri/index.tsx
+++ src/routes/demo/travelyatri/index.tsx
@@ -1,14 +1,9 @@
-import Navbar from "~/components/home/navbar"
+import Body from "~/components/bodyy/body"
 
 export default function index() {
   return (
     <>
-    <div class="flex flex-col min-h-screen bg-gradient-to-r from-slate-900 to-slate-700">
-  <Navbar></Navbar>
-   
-    <div class="flex grow justify-center items-center flex-col gap-3">
-    </div>
-  </div>
+   <Body></Body>
       
     </>
   )
